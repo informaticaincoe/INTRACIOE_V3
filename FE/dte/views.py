b8e18f8b7491b4de76dc6a83a4da4bba229ea2b3
brenda-incoeit <brenda.chacon@grupoincoe.com>
2025-03-04 08:53:27 -0600

    soporte consumidor final
    soporte consumidor final

--- FE/dte/views.py
+++ FE/dte/views.py
@@ -47,6 +47,9 @@ CERT_PATH = "FE/cert/06142811001040.crt"  # Ruta al certificado
 HACIENDA_URL_TEST = "https://apitest.dtes.mh.gob.sv/fesv/recepciondte"
 HACIENDA_URL_PROD = "https://api.dtes.mh.gob.sv/fesv/recepciondte"
 
+#BC 04/03/2025: Constantes
+COD_CONSUMIDOR_FINAL = "01"
+
 #vistas para actividad economica
 def cargar_actividades(request):
     if request.method == 'POST':
@@ -275,7 +278,7 @@ def generar_factura_view(request):
             telefono_receptor = data.get('telefono_receptor', '')
             correo_receptor = data.get('correo_receptor', '')
             observaciones = data.get('observaciones', '')
-            tipo_dte = data.get("tipo_documento", None)
+            tipo_dte = data.get("tipo_documento", None) #BC: obtiene la seleccion del tipo de documento desde la pantalla del sistema
 
             # Configuración adicional
             tipooperacion_id = data.get("condicion_operacion", None)
@@ -322,7 +325,8 @@ def generar_factura_view(request):
 
             # Configuración por defecto de la factura
             ambiente_obj = Ambiente.objects.get(codigo="01")
-            tipo_dte_obj = Tipo_dte.objects.get(codigo="01")
+            tipo_dte_obj = Tipo_dte.objects.get("tipo_documento")
+            print(f"Tipo de documento a generar = {tipo_dte_obj}")
             tipomodelo_obj = Modelofacturacion.objects.get(codigo="1")
             tipooperacion_obj = CondicionOperacion.objects.get(id=tipooperacion_id) if tipooperacion_id else None
             tipo_moneda_obj = TipoMoneda.objects.get(codigo="USD")
@@ -365,14 +369,21 @@ def generar_factura_view(request):
 
                 # Calcular precio neto y IVA unitario
                 #precio_neto = (precio_incl / Decimal("1.13")).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-                precio_neto = (precio_incl ).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-                print(f"Precio Incl = {precio_incl}, Precio neto = {precio_neto} ")
+                if tipo_dte == COD_CONSUMIDOR_FINAL :
+                    precio_neto = (precio_incl ).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+                    total_iva_item = ( ( precio_incl * cantidad) / Decimal("1.13") * Decimal("0.13") ).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+                else:  #Cuando no es FE quitarle iva al precio
+                    precio_neto = (precio_incl / Decimal("1.13")).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+                    #BC: verificar si el precio del prod para ccf ya viene con iva
+                    total_iva_item = ( ( precio_incl * cantidad) / Decimal("1.13") * Decimal("0.13") ).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+                print(f"Precio Incl = {precio_incl}, Precio neto = {precio_neto}, tipo dte:  {tipo_dte}")
+                
                 iva_unitario = (precio_incl - precio_neto).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
 
                 # Totales por ítem
                 total_neto = (precio_neto * cantidad).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
                 #total_iva_item = (iva_unitario * cantidad).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-                total_iva_item = ( ( precio_incl * cantidad) / Decimal("1.13") * Decimal("0.13") ).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+                #funcional total_iva_item = ( ( precio_incl * cantidad) / Decimal("1.13") * Decimal("0.13") ).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
                 
                 print(f"IVA Item = {total_iva_item}, iva unitario = {iva_unitario}, cantidad = {cantidad} ")
                 
