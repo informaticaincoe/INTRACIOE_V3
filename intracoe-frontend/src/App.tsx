94649c85b90c760ca64499be019aeec0dfaad98c
Karen-Burgos <karen.burgos@grupoincoe.com>
2025-03-11 13:48:27 -0600

    manejo de errores en formulario de login

--- intracoe-frontend/src/App.tsx
+++ intracoe-frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Login } from './features/login/pages/login';
+import { Login } from './features/login/pages/loginPage';
 
 function App() {
   return (
--- intracoe-frontend/src/features/login/components/loginForm.tsx
+++ intracoe-frontend/src/features/login/components/loginForm.tsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
 import { Input } from '../../../shared/forms/input';
+import { SendFormButton } from '../../../shared/buttons/sendFormButton';
 
 export const LoginForm = () => {
   const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+  // const [error, setError] = useState<string | null>(null);
+  const [errors, setErrors] = useState({
     username: '',
     password: '',
   });
@@ -11,9 +17,29 @@ export const LoginForm = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const handlerForm = (e:React.FormEvent) => {
+    e.preventDefault();
+    
+
+    let newErrors = { username: '', password: '' };
+
+    if (!formData.username) {
+      newErrors.username = 'Ingrese su usuario';
+    }
+    if (!formData.password) {
+      newErrors.password = 'Ingrese su contraseña';
+    }
+
+    setErrors(newErrors);
+
+    if (!newErrors.username && !newErrors.password) {
+      console.log(formData.username, formData.password);
+    }
+  }
+
   return (
-    <form className='flex flex-col gap-10'>
-      <span>
+    <form className="flex flex-col gap-10 w-full">
+      <span className="flex flex-col gap-5">
         <span className="flex flex-col items-start">
           <label htmlFor="username">Usuario</label>
           <Input
@@ -23,19 +49,26 @@ export const LoginForm = () => {
             value={formData.username}
             onChange={handleChange}
           />
+          {errors.username && (
+            <span className="text-red-500 text-sm">{errors.username}</span>
+          )}
         </span>
+
         <span className="flex flex-col items-start">
           <label htmlFor="password">Contraseña</label>
           <Input
             name="password"
             placeholder="Contraseña"
-            type="text"
-            value={formData.username}
+            type="password"
+            value={formData.password}
             onChange={handleChange}
           />
+          {errors.password && (
+            <span className="text-red-500 text-sm">{errors.password}</span>
+          )}
         </span>
       </span>
-      <button type="button" className='bg-primary-yellow text-white w-full py-3'>Ingresar</button>
+      <SendFormButton text='Ingresar' onClick={handlerForm}/>
     </form>
   );
 };
