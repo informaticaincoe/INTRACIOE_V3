4f8c2d4ab44836df6cf711a9d4f6fd088619781b
Karen-Burgos <karen.burgos@grupoincoe.com>
2025-03-27 07:46:34 -0600

    Corregir tabla resumen en genracion de factura

--- intracoe-frontend/src/features/facturacion/generateDocuments/components/Shared/resumenTotales/resumenTotalesCard.tsx
+++ intracoe-frontend/src/features/facturacion/generateDocuments/components/Shared/resumenTotales/resumenTotalesCard.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ProductosTabla } from '../../FE/productosAgregados/productosData'
 import { InputNumber, InputNumberValueChangeEvent } from 'primereact/inputnumber'
 
 interface ResumenTotalesCardProps {
     listProducts: ProductosTabla[],
     setDescuentoGeneral: any,
-    descuentoGeneral:number
+    descuentoGeneral: number
 }
 
 export const ResumenTotalesCard: React.FC<ResumenTotalesCardProps> = ({ listProducts, setDescuentoGeneral, descuentoGeneral }) => {
+
+    useEffect(() => {
+        console.log("descuento", listProducts)
+    }, [])
+
+    const CalcularTotalIVA = () => {
+        let aux = 0;
+        listProducts.forEach((item) => {
+            aux += item.cantidad * item.iva_unitario
+        })
+
+        return aux
+    }
 
     const CalcularTotalNeto = () => {
         let aux = 0
@@ -26,22 +39,29 @@ export const ResumenTotalesCard: React.FC<ResumenTotalesCardProps> = ({ listProd
         return aux
     }
 
+    const CalcularTotalConIVA = () => {
+        let aux = 0
+        aux += CalcularTotalNeto() + CalcularTotalIVA() 
+        
+        return aux
+    }
+
     return (
         <div className="grid grid-cols-[auto_1fr_auto_1fr] gap-4 text-start">
             <p className="opacity-60">SubTotal Neto:</p>
             <p>${CalcularTotalNeto()}</p>
 
             <p className="opacity-60">Total IVA:</p>
-            <p>$2.78</p>
+            <p>${CalcularTotalIVA()}</p>
 
             <p className="opacity-60">Total con IVA:</p>
-            <p>$21.24</p>
+            <p>${CalcularTotalConIVA()}</p>
 
             <p className="opacity-60">Monto descuento:</p>
             <p>$ {CalcularDescuento()}</p>
 
             <p className='opacity-60'>Total a pagar:</p>
-            <p>$21.60</p>
+            <p>{CalcularTotalConIVA()}</p>
 
             <p className='opacity-60'>Descuento general:</p>
             <InputNumber
@@ -50,7 +70,7 @@ export const ResumenTotalesCard: React.FC<ResumenTotalesCardProps> = ({ listProd
                 onValueChange={(e: InputNumberValueChangeEvent) =>
                     setDescuentoGeneral(e.value ?? 0)
                 }
-                style={{padding:0, height:'2rem'}}
+                style={{ padding: 0, height: '2rem' }}
             />
         </div>
     )
