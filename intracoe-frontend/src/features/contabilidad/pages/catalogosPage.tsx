dbb3f5b3a442dc9dc596db4f24c002d311baebfe
Karen-Burgos <karen.burgos@grupoincoe.com>
2025-04-10 15:10:02 -0600

    mostrar catalogo actividades economicas

--- intracoe-frontend/src/features/contabilidad/pages/catalogosPage.tsx
+++ intracoe-frontend/src/features/contabilidad/pages/catalogosPage.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react"
-import { WhiteSectionsPage } from "../../../shared/containers/whiteSectionsPage"
 import { Title } from "../../../shared/text/title"
-import { ActivitiesPage } from "../../facturacion/activities/pages/activitiesPage"
 import { TableContainer } from "../../facturacion/activities/components/activitiesTable/tableContainer"
 
 export const CatalogosPage = () => {
@@ -11,18 +9,20 @@ export const CatalogosPage = () => {
     <>
       <Title text="Catalogos" />
 
-        <div className="grid grid-cols-[15%_85%] gap-10 p-10 h-full">
+        <div className="grid grid-cols-[18%_82%] gap-10 p-10 h-full">
           <div className="text-start bg-white py-5 px-5">
             <h2 className="font-semibold text-xl pb-5">Lista catalogos</h2>
-            <ul className=" h-full flex flex-col gap-5">
-              <li onClick={() => setshowSideMenu(!showSideMenu)} className="bg-blue-200 p-2 rounded-md">Catalogo</li>
-              <li className="px-2">Descuentos</li>
-              <li className="px-2">Ambientes</li>
-              <li className="px-2">Plazos</li>
+            <div className="h-[1px] w-full bg-gray-200 mb-5"></div>
+            <ul className=" h-full flex flex-col gap-2">
+              <li onClick={() => setshowSideMenu(!showSideMenu)} className="bg-blue-200 px-2 py-1 rounded-md">Catalogo</li>
+              <li className="px-2 py-1">Descuentos</li>
+              <li className="px-2 py-1">Ambientes</li>
+              <li className="px-2 py-1">Plazos</li>
             </ul>
           </div>
           <div className="justify-center bg-white p-5">
               <TableContainer />
+
           </div>
         </div>
     </>
