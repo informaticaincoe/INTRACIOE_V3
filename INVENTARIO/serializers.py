b6d49b8c3f71c73e80cc12395a9d6d8249504be9
Francisco Antonio Flores <informatica@grupoincoe.com>
2025-04-11 13:12:10 -0600

    CATALOGO
    API DE CATALOGO

--- INVENTARIO/serializers.py
+++ INVENTARIO/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from FE.models import Descuento
-from .models import Almacen, Impuesto, Producto, TipoItem, TipoTributo, TipoUnidadMedida, Tributo
+from .models import Almacen, Impuesto, Producto, TipoItem, TipoTributo, TipoUnidadMedida, Tributo, Proveedor, Compra, DetalleCompra, MovimientoInventario, AjusteInventario, DevolucionCompra, DevolucionVenta, DetalleDevolucionCompra, DetalleDevolucionVenta
 
 
 class ProductoSerializer(serializers.ModelSerializer):
@@ -25,7 +25,6 @@ class TiposTributosSerializer(serializers.ModelSerializer):
         model = TipoTributo
         fields = '__all__'
 
-
 class TributosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tributo
@@ -44,4 +43,54 @@ class ImpuestoSerializer(serializers.ModelSerializer):
 class AlmacenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Almacen
+        fields = '__all__'
+
+class ProveedorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proveedor
+        fields = '__all__'
+
+class CompraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Compra
+        fields = '__all__'
+        read_only_fields = ['fecha']  # Fecha generada automáticamente
+
+class DetalleCompraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DetalleCompra
+        fields = '__all__'
+
+class MovimientoInventarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovimientoInventario
+        fields = '__all__'
+        read_only_fields = ['fecha']
+
+class AjusteInventarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AjusteInventario
+        fields = '__all__'
+        read_only_fields = ['fecha']
+
+class DevolucionVentaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DevolucionVenta
+        fields = '__all__'
+        read_only_fields = ['fecha']
+
+class DetalleDevolucionVentaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DetalleDevolucionVenta
+        fields = '__all__'
+
+class DevolucionCompraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DevolucionCompra
+        fields = '__all__'
+        read_only_fields = ['fecha']
+
+class DetalleDevolucionCompraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DetalleDevolucionCompra
         fields = '__all__'
