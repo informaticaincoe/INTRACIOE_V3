a325655e104785e35f1af9ee2a17577d0e574de7
Francisco Antonio Flores <informatica@grupoincoe.com>
2025-04-24 08:50:49 -0600

    logout
    LOGOUT

--- AUTENTICACION/urls_api.py
+++ AUTENTICACION/urls_api.py
@@ -1,13 +1,15 @@
 
 from django.urls import path
+
+from AUTENTICACION.serializers import logoutAPIView
 from .api_views import ChangePasswordAPIView, LoginAPIView, PasswordResetRequestAPIView, PasswordResetConfirmAPIView
 
 
 urlpatterns = [
 
     path('login/', LoginAPIView.as_view(), name='api-login'),
+    path('logout/', logoutAPIView.as_view(), name='api-logout'),
     path('change-password/', ChangePasswordAPIView.as_view(), name='api-change-password'),
-
     path('password-reset/', PasswordResetRequestAPIView.as_view(), name='api-password-reset'),
     path('password-reset/confirm/', PasswordResetConfirmAPIView.as_view(), name='api-password-reset-confirm'),
 
