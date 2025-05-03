6e80f10fa748c27711f2699cb3ba3b0f2ee67489
Karen-Burgos <karen.burgos@grupoincoe.com>
2025-05-03 10:06:22 -0600

    POST formulario y cambio de color en tarjeta

--- intracoe-frontend/src/features/POST/components/cardModal.tsx
+++ intracoe-frontend/src/features/POST/components/cardModal.tsx
@@ -1,8 +1,20 @@
 import { Dialog } from 'primereact/dialog';
 import React, { useState } from 'react'
 import { CardTemplate } from './cardTemplate';
-import { Input } from '../../../shared/forms/input';
-import { FaCcMastercard } from "react-icons/fa6";
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
+import { InputText } from "primereact/inputtext";
+import { IoPersonOutline } from 'react-icons/io5';
+import mastercard from "../../../assets/mastercardIcon.svg"
+import visa from "../../../assets/visa.svg"
+
+import password from "../../../assets/password.svg"
+import { Calendar } from 'primereact/calendar';
+import { InputMask } from "primereact/inputmask";
+import { Password } from 'primereact/password';
+import { CiCalendar } from 'react-icons/ci';
+import dayjs from 'dayjs';
+
 
 interface CardModalProps {
     visible: boolean,
@@ -14,49 +26,92 @@ export const CardModal: React.FC<CardModalProps> = ({ visible, setVisible }) => 
     const [formData, setFormData] = useState({
         cardName: "",
         cardNumber: "",
-        expiredDateMonth: "",
-        expiredDateYear: "",
+        expiredDate: undefined,
         CVV: ""
     })
 
     const handleChangeForm = (e: any) => {
-        setFormData({ ...formData, [e.target.name]: [e.target.value] })
+        const { name, value } = e.target;
+
+        // Si el campo es la fecha de vencimiento, formateamos la fecha a 'MM/YY' usando dayjs
+        if (name === 'expiredDate' && value) {
+            const formattedDate = dayjs(value).format('MM/YY'); // Formateamos la fecha a 'MM/YY'
+            setFormData({ ...formData, [name]: formattedDate });
+        } else {
+            setFormData({ ...formData, [name]: value });
+        }
     }
 
     const handlerForm = (e: React.FormEvent) => {
         e.preventDefault();
+
+
     }
     return (
         <Dialog header={<p>POST</p>} visible={visible} onHide={() => { if (!visible) return; setVisible(false); }}
-            style={{ width: '40vw' }} breakpoints={{ '960px': '75vw', '641px': '100vw' }}>
+            style={{ width: '35vw' }} breakpoints={{ '1679': '30vw', '1462px': '45vw', }}>
 
-            <div>
-                <CardTemplate nameCard={formData.cardName} cardNumber={formData.cardNumber} expiredDateMonth={formData.expiredDateMonth} expiredDateYear={formData.expiredDateYear}></CardTemplate>
+            <div className='px-8'>
+                <CardTemplate nameCard={formData.cardName} cardNumber={formData.cardNumber} expiredDate={formData.expiredDate}></CardTemplate>
 
                 <form action="" className='flex flex-col gap-5 py-10'>
                     <span>
                         <label htmlFor="cardName" className='opacity-70'>Nombre tarjeta</label>
-                        <Input name="cardName" value={formData.cardName} onChange={(e: any) => handleChangeForm(e)} />
+                        <IconField iconPosition="left">
+                            <InputIcon> <IoPersonOutline /></InputIcon>
+                            <InputText placeholder="Nombre apellido" style={{ width: '100%', paddingLeft: '3rem' }} name="cardName" onChange={(e: any) => handleChangeForm(e)} />
+                        </IconField>
                     </span>
-                    <div className='flex'>
-                        <span  className='flex flex-col w-full'>
+                    <div className='flex gap-5'>
+                        <span className='flex flex-col w-full'>
                             <label htmlFor="cardNumber" className='opacity-70'>Numero tarjeta</label>
-                            <Input name="cardNumber" value={formData.cardNumber} onChange={(e: any) => handleChangeForm(e)} className='w-full' />
+                            <IconField iconPosition="left">
+                                <InputIcon>
+                                    {
+                                        (formData.cardNumber[0] == '4') ?
+                                            <embed src={visa} className='w-7' />
+                                            :
+                                            <embed src={mastercard} className='h-3' />
+                                    }
+
+                                </InputIcon>
+                                <InputMask mask="9999 9999 9999 9999" placeholder="0000 0000 0000 0000" style={{ width: '100%', padding:'2% 8%' }} name="cardNumber" value={formData.cardNumber} onChange={(e) => handleChangeForm(e)}></InputMask>
+                            </IconField>
                         </span>
-                        
                     </div>
                     <div className='flex gap-5'>
                         <span>
-                            <label htmlFor="expiredDateMonth" className='opacity-70'>Mes</label>
-                            <Input name="expiredDateMonth" value={formData.expiredDateMonth} onChange={(e: any) => handleChangeForm(e)} />
-                        </span><span>
-                            <label htmlFor="expiredDateYear" className='opacity-70'>Año</label>
-                            <Input name="expiredDateYear" value={formData.expiredDateYear} onChange={(e: any) => handleChangeForm(e)} />
+                            <label htmlFor="expiredDate" className='opacity-70'>Fecha vencimiento</label>
+
+                            <Calendar
+                                name="expiredDate"
+                                value={formData.expiredDate ? dayjs(formData.expiredDate, 'MM/YY').toDate() : null} // Convertimos la fecha en formato MM/YY
+                                onChange={handleChangeForm}
+                                dateFormat="mm/yy"
+                                view='month'
+                                showIcon
+                                icon={<CiCalendar size={20} color='rgba(0,0,0,0.5)' />}
+                                iconPos="left"
+
+                            />
                         </span>
                         <span>
                             <label htmlFor="CVV" className='opacity-70'>CVV</label>
-                            <Input type='password' name="CVV" value={formData.CVV} onChange={(e: any) => handleChangeForm(e)} />
+                            <IconField iconPosition="left">
+                                <InputIcon>
+                                    <embed src={password} className='h-5 flex items-center opacity-60' />
+
+                                </InputIcon>
+                                <Password
+                                    name="CVV"
+                                    value={formData.CVV}
+                                    panelStyle={{ display: 'none' }}
+                                    onChange={(e: any) => handleChangeForm(e)}
+                                    toggleMask
+                                />
+                            </IconField>
                         </span>
+
                     </div>
                 </form>
             </div>
--- intracoe-frontend/src/features/POST/components/cardTemplate.tsx
+++ intracoe-frontend/src/features/POST/components/cardTemplate.tsx
@@ -1,44 +1,89 @@
 import React from 'react'
-import { FaCcMastercard } from 'react-icons/fa6'
-import { LuWifi } from 'react-icons/lu'
+import chip from "../../../assets/chip.svg"
+import mastercard from "../../../assets/mastercard.svg"
+import visa from "../../../assets/visa.svg"
 
 interface CardTemplateProps {
     nameCard: string,
     cardNumber: string,
-    expiredDateMonth: string,
-    expiredDateYear: string
+    expiredDate: any,
 }
 
-export const CardTemplate: React.FC<CardTemplateProps> = ({ nameCard, cardNumber, expiredDateMonth, expiredDateYear }) => {
+export const CardTemplate: React.FC<CardTemplateProps> = ({ nameCard, cardNumber, expiredDate = "" }) => {
+
+    const cardType = () => {
+        if (cardNumber[0] == '4')
+            return true
+        else
+            return false
+    }
+
     return (
         <div
-            className='relative text-white rounded-md px-10 py-5 h-60 flex flex-col justify-between'
+            className="relative text-white rounded-md gap-15 px-10 py-5 h-[100%] flex flex-col justify-between overflow-hidden"
+
+
             style={{
-                backgroundColor: 'hsla(263,96%,59%,1)',
+                backgroundColor: 'hsla(240,63%,67%,1)',
                 backgroundImage: `
-                    radial-gradient(at 19% 61%, hsla(209,65%,73%,1) 0px, transparent 50%),
-                    radial-gradient(at 49% 47%, hsla(278,95%,75%,1) 0px, transparent 50%),
-                    radial-gradient(at 6% 12%, hsla(298,99%,66%,1) 0px, transparent 50%),
-                    radial-gradient(at 26% 16%, hsla(28,60%,78%,1) 0px, transparent 50%),
-                    radial-gradient(at 86% 90%, hsla(286,95%,84%,1) 0px, transparent 50%)`
+                    radial-gradient(at 99% 24%, hsla(239,82%,74%,1) 0px, transparent 50%),
+                    radial-gradient(at 3% 99%, hsla(256,88%,66%,1) 0px, transparent 50%),
+                    radial-gradient(at 2% 97%, hsla(298,70%,87%,1) 0px, transparent 50%),
+                    radial-gradient(at 42% 92%, hsla(245,100%,51%,1) 0px, transparent 50%),
+                    radial-gradient(at 86% 80%, hsla(266,100%,87%,1) 0px, transparent 50%),
+                    radial-gradient(at 46% 32%, hsla(273,84%,84%,1) 0px, transparent 50%),
+                    radial-gradient(at 31% 81%, hsla(146,92%,67%,1) 0px, transparent 50%),
+                    radial-gradient(at 96% 52%, hsla(151,83%,65%,1) 0px, transparent 50%)
+                    `
             }}>
             {/* -           <div className='bg-white w-full h-10 opacity-40 absolute -left-10' ></div> */}
             {/* <div className="absolute inset-x-0 top-5 h-7 bg-white opacity-40"></div> */}
 
 
             <div className='flex justify-between'>
-                <h1 className='font-bold font-5xl'>Datos tarjeta</h1>
+                {/* <FcSimCardChip size={24} className='text-black'/> */}
+                <img src={chip} alt="" className='h-10 ' />
+                {
+                    (cardNumber[0] == '4') ?
+                        <img src={visa} alt="" className='h-7 z-20' />
+                        :
+                        <img src={mastercard} alt="" className='h-10 z-20' />
+
+                }
+
             </div>
+            <span>
+                <p className='text-3xl font-bold'>{cardNumber || "**** **** **** 1212"}</p>
+            </span>
             <div className='flex justify-between'>
                 <span>
-                    <p>{nameCard || "Nombre Apellidos"}</p>
-                    <p className='text-2xl font-bold'>{cardNumber || "1234 5678 9101 1212"}</p>
+                    <p className='text-sm'>Nombre tarjeta</p>
+                    <p className='text-xl'>{nameCard || "Nombre apellido"}</p>
+
                 </span>
                 <span>
-                    <p>{expiredDateMonth || "04"}/{expiredDateYear || "25"}</p>
-                    <FaCcMastercard size={35} className='opacity-70' />
+                    <p className='text-sm'>Fecha vencimiento</p>
+                    <p>{expiredDate || "MM/YY"}</p>
                 </span>
+                <div className='inset-x-90 -inset-y-40 size-80 absolute bg-white z-10 opacity-20 rounded-full '></div>
+                <div className='inset-x-60 -inset-y-20 size-50 absolute bg-white z-10 opacity-10 rounded-full '></div>
+
+                <div className='-inset-x-40 inset-y-30 size-100 absolute bg-white z-10 opacity-20 rounded-full '></div>
+
             </div>
         </div>
     )
 }
+
+
+
+// backgroundColor: 'hsla(240,63%,67%,1)',
+//                 backgroundImage: `
+//                     radial-gradient(at 99% 24%, hsla(239,82%,74%,1) 0px, transparent 50%),
+// radial-gradient(at 3% 99%, hsla(256,88%,66%,1) 0px, transparent 50%),
+// radial-gradient(at 2% 97%, hsla(298,70%,87%,1) 0px, transparent 50%),
+// radial-gradient(at 42% 92%, hsla(245,100%,51%,1) 0px, transparent 50%),
+// radial-gradient(at 86% 80%, hsla(266,100%,87%,1) 0px, transparent 50%),
+// radial-gradient(at 46% 32%, hsla(273,84%,84%,1) 0px, transparent 50%),
+// radial-gradient(at 31% 81%, hsla(146,92%,67%,1) 0px, transparent 50%),
+// radial-gradient(at 96% 52%, hsla(151,83%,65%,1) 0px, transparent 50%)
