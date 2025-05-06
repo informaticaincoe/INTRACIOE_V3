48a730ceec43a304c12360fe9023b5744788776f
Karen-Burgos <karen.burgos@grupoincoe.com>
2025-05-06 14:02:01 -0600

    Footer desarrollado por intracoe

--- intracoe-frontend/src/shared/footer/footer.tsx
+++ intracoe-frontend/src/shared/footer/footer.tsx
@@ -2,12 +2,7 @@ export const Footer = () => {
   return (
     <footer className="bg-footer">
       <p className="text-light-text py-3 font-medium">
-        Copyright ©2025 Todos los derechos reservados | Este sistema esta
-        desarrollado por{' '}
-        <a href="https://www.incoe.net/" className="underline">
-          incoe.net
-        </a>
-      </p>
+        Copyright ©2025 Todos los derechos reservados | Este sistema esta desarrollado por intracoe</p>
     </footer>
   );
 };
